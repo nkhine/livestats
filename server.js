10b91bb95c298f6d2fb4663ebdef4ad4cc2ebd58
Norman Khine <khinester@aqoon.local>
2010-10-07 15:30:30 +0200

    00:29:16

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 require.paths.unshift(__dirname + "/vendor");
 
-var http = require('http'),
-	sys = require('sys'),
-	nodeStatic = require('node-static/lib/node-static');
-	
-var file = new(nodeStatic.Server)('./public', {cache: false});	
-var server = http.createServer(function(request, response) {
-	request.addListener('end', function() {
-		file.serve(request, response);
-	})
-})
-server.listen(8124);
+var LiveStats = require('./lib/livestats');
+
+new LiveStats({
+	port: 8000,
+	geoipServer: {
+		hostname: 'geoip.peepcode.com'
+		, port: 80
+	}
+})	
+
