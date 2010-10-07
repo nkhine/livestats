918f346cf8853bb2b4b64405dc1135aa820f0698
Norman Khine <khinester@aqoon.local>
2010-10-07 15:52:24 +0200

    00:32:00 end of error chapter

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 require.paths.unshift(__dirname + "/vendor");
 
+process.addListener('uncoughtException', function(err, stack) {
+	console.log('------------------');
+	console.log('Exception ' + err);
+	console.log(err.stack);
+	console.log('------------------');
+};
+	
+	);
 var LiveStats = require('./lib/livestats');
 
 new LiveStats({
